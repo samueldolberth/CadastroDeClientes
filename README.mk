e8181bbb45dcdcf014dcf7157e047c7d9580d9a0
samueldolberth <samudolberth@gmail.com>
2025-08-30 00:30:41 -0300

    add .mk file

---
+++ README.mk
@@ -0,0 +1,6 @@
+# SISTEMA DE CADASTRO DE CLIENTES #
+Feito com python e flask
+
+Com o objetivo de aprofundar conhecimentos e obter experiencia com as ferramentas e libs
+
+Material de estudo: playlist "Desenvolvimento Web com Flask e Python" do Programador Python no Youtube
