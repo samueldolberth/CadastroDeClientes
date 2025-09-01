2533d16448ac54921d87c7577f17ec9fb0c974ea
samueldolberth <samudolberth@gmail.com>
2025-09-01 01:16:42 -0300

    update, edit clientes

--- routes/cliente.py
+++ routes/cliente.py
@@ -8,6 +8,8 @@ def lista_cliente():
     """ listar os clientes """
     return render_template('lista_clientes.html', clientes=CLIENTES)
 
+
+
 @cliente_route.route('/', methods=['POST'])
 def inserir_cliente():
     """ inserir os dados do cliente """
@@ -23,30 +25,56 @@ def inserir_cliente():
     CLIENTES.append(novo_usuario)
     return render_template('item_cliente.html', cliente=novo_usuario)
 
+
+
+
 @cliente_route.route('/new')
 def form_cliente():
     """ formulario para cadastrar um cliente"""
     return render_template('form_cliente.html')
+
+
 
 @cliente_route.route('/<int:cliente_id>')
 def detalhe_cliente(cliente_id):
     """ formulario para cadastrar um cliente"""
     return render_template('detalhe.html')
 
+
+
 @cliente_route.route('/<int:cliente_id>/edit')
 def form_edit_cliente(cliente_id):
     """ formulario para editar um cliente"""
+
+    # obtem usuario pelo id
     cliente = None
     for c in CLIENTES:
-        if c['id'] == cliente.id:
+        if c['id'] == cliente_id:
             cliente = c
 
     return render_template('form_cliente.html', cliente=cliente)
 
+
+
 @cliente_route.route('/<int:cliente_id>/update', methods=['PUT'])
 def atualizar_cliente(cliente_id):
     """ atualizar informações do cliente """
-    pass
+    cliente_editado = None
+    # obter dados do formulario de edição
+    data = request.json
+
+    # obter usuario pelo id
+    for c in CLIENTES:
+        if c['id'] == cliente_id:
+            c['nome'] = data['nome']
+            c['email'] = data['email']
+
+            cliente_editado = c
+
+    # editar usuario
+    return render_template('item_cliente.html', cliente=cliente_editado)
+
+
 
 @cliente_route.route('/<int:cliente_id>/delete', methods=['DELETE'])
 def deletar_cliente(cliente_id):
